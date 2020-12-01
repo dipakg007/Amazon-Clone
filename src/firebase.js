62d981c3c58fc5c6d5b8239b26a774fd9a1f26a8
Dipak Gupta <dipakg286@gmail.com>
2020-12-01 23:53:06 +0530

    Day1

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQDeamoApWa_s2pZSqjgHm8KRQ0gaW4lQ",
+  authDomain: "clone-1e362.firebaseapp.com",
+  databaseURL: "https://clone-1e362.firebaseio.com",
+  projectId: "clone-1e362",
+  storageBucket: "clone-1e362.appspot.com",
+  messagingSenderId: "493612816902",
+  appId: "1:493612816902:web:b85ac820c883f4e42b4187",
+  measurementId: "G-243EKLE7PS",
+};
