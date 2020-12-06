618b4e3266b0d98a7d5c0546979a36ce099ce326
Dipak Gupta <dipakg286@gmail.com>
2020-12-06 21:41:29 +0530

    Subtotal and contextApi

--- src/Home.js
+++ src/Home.js
@@ -13,12 +13,14 @@ function Home() {
         />
         <div className="home__row">
           <Product
+            id={1}
             title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
             price={1441}
             image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
             rating={4}
           />
           <Product
+            id={2}
             title="Spies, Lies & Red Tape : A Spy-Military-Political Fiction Thriller based on the Indian Subcontinent: A Spy-Military-Political Fiction Thriller based on the Indian Subcontinent"
             price={299}
             image="https://m.media-amazon.com/images/I/41Lo4TOqo3L.jpg"
@@ -27,18 +29,21 @@ function Home() {
         </div>
         <div className="home__row">
           <Product
+            id={3}
             title="boAt Rockerz 255 Pro in-Ear Bluetooth Neckband Earphone with Mic(Navy Blue)"
             price={1449}
             image="https://images-na.ssl-images-amazon.com/images/I/61IGr%2BQK6yL._SL1500_.jpg"
             rating={5}
           />
           <Product
+            id={4}
             title="Samsung Galaxy M51 (Electric Blue, 8GB RAM, 128GB Storage)"
             price={24999}
             image="https://images-na.ssl-images-amazon.com/images/I/710weRkP-nL._SL1500_.jpg"
             rating={1}
           />
           <Product
+            id={5}
             title="All-new Echo Dot (4th Gen) | Next generation smart speaker with improved bass and Alexa (Black)"
             price={4499}
             image="https://images-na.ssl-images-amazon.com/images/I/61KIy6gX-CL._SL1000_.jpg"
@@ -47,6 +52,7 @@ function Home() {
         </div>
         <div className="home__row">
           <Product
+            id={6}
             title="Samsung 34-inch (86.40cm) Curved Monitor- 21:9 Ultrawide QLED, Thunderbolt 3 Port- LC34J791WTWXXL"
             price={81500}
             image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    let _total = 0;
+    basket.map((item) => (_total += item.price));
+    setTotal(_total);
+  }, [basket]);
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
-              <strong>&#8377;0</strong>
+              Subtotal ({basket?.length} items):
+              <strong>&#8377;{total}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
